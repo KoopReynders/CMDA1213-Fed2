baf5f60259c302d6e321bf3531c9ce1653b94dd1
joost <joost@maximonster.com>
2012-12-02 20:04:20 +0100

    Tweaked backbone sample app

--- Les 3/Lesopdrachten/Backbone Tournament/js/app/app.js
+++ Les 3/Lesopdrachten/Backbone Tournament/js/app/app.js
@@ -1,8 +1,9 @@
 ﻿$(function() {
  	//Tournament data
     var tournament = { 
-		name: "Threesomes", 
-		info: "Lorem ipsum dolor sit amet" 
+		name: "Threesome Ultimate Tournament",
+		scheduling_format:'regular', 
+		info: "Threesome Ultimate Tournament is a Mini Ultimate tournament played in Amsterdam. Games are played 3 on 3. Your team can contain a maximum of 4 players."
 	}
 
     //create instance of tournamentView
