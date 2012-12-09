e8a08b05543bf3f362f879c363d9db498720ad7b
joost <joost@maximonster.com>
2012-12-09 21:21:55 +0100

    added docco for les 3

--- Les 3/Lesopdrachten/Backbone Tournaments/js/app/app.js
+++ Les 3/Lesopdrachten/Backbone Tournaments/js/app/app.js
@@ -1,64 +1,131 @@
-﻿(function ($) {
+﻿//Tournaments Web App *(Backbone example application)*
+/**
+ *	Tournaments Web App (Backbone example application)
+ *	
+ *
+ */
 
-    // Tournament data
+// Anonymous self-invoked function with jQuery mapped to $
+(function ($) {
+
+    // # League data #
     leagueData = [
         { name: "Threesome Ultimate Tournament", schedulingFormat:"regular"},
 		{ name: "Wisconsin Swiss 2012", schedulingFormat:"swiss"},
 		{ name: "New York City PoNY", schedulingFormat:"regular"},
-		{ name: "Swiss Chalet", schedulingFormat:"swiss"}
+		{ name: "Swiss Chalet"}
     ];
 
-	// define tournament model
+	// # Define tournament model #
 	Tournament = Backbone.Model.extend({
-		defaults: function () {
-			
+		// Set model defaults *(backbone method)*
+		defaults: {
+			"name": "Tournament name unknown",
+			"schedulingFormat":"unknown"
+		},
+		
+		// Initialize model *(backbone method)*
+		initialize: function () {
+			this.logMessage("Tournament model initialized");
+		},
+		
+		// Log message *(custom method)*
+		logMessage: function (message) {
+			console.log(message);
 		}
 	});
 
-	// define tournaments collection
+	// # Define league collection #
 	League = Backbone.Collection.extend({
-	    model: Tournament
+	    // Specifiy model for this collection
+		model: Tournament,
+		
+		// Initialize collection *(backbone method)*
+		initialize: function () {
+			this.logMessage("League collection initialized");
+		},
+		
+		// Log message *(custom method)*
+		logMessage: function (message) {
+			console.log(message);
+		}
 	});
 	
-	// define individual tournament view
+	// # Define tournament view #
 	TournamentView = Backbone.View.extend({
-	    tagName: "li",
+	    // Define element (this.el)  
+		tagName: "li",
+		
+		// Set reference to template
 	    template: $("#tournamentTemplate").html(),
-
+		
+		// Initialize view *(backbone method)*
+		initialize: function () {
+			this.logMessage("Tournament view initialized");
+		},
+		
+		// Render view *(backbone method)*
 	    render: function () {
+			// Store template in variable
 	        var tmpl = _.template(this.template);
-
+			
+			// Inject the rendered tempate into the views element 
 	        $(this.el).html(tmpl(this.model.toJSON()));
-	        return this;
-	    }
+	
+			return this;
+	    },
+	
+		// Log message *(custom method)*
+		logMessage: function (message) {
+			console.log(message);
+		}
 	});
 	
-	// define tournaments view
+	// # Define league view #
 	LeagueView = Backbone.View.extend({
-	    el: $("#league"),
-
+		// Define element (this.el)     
+		el: $("#league"),
+		
+		// Initialize view *(backbone method)*
 	    initialize: function () {
-	        this.collection = new League(leagueData);
+			this.logMessage("League view initialized");
+	        
+			// Specify collection for this view
+			this.collection = new League(leagueData);
+			
+			// Render view
 	        this.render();
+			
 	    },
-
+		
+		// Render view *(backbone method)*
 	    render: function () {
-	        var that = this;
+	        var self = this;
 
 	        _.each(this.collection.models, function (item) {
-	            that.renderTournament(item);
+	            self.renderTournament(item);
 	        }, this);
 	    },
-
+		
+		// Render tournament *(custom method)*
 	    renderTournament: function (item) {
-	        var tournamentView = new TournamentView({
+			// Create new instance of TournamentView
+			var tournamentView = new TournamentView({
 	            model: item
 	        });
+	
+			// Append the rendered HTML to the views element
 	        this.$el.append(tournamentView.render().el);
-	    }
+	    },
+	
+		// Log message *(custom method)*
+		logMessage: function (message) {
+			console.log(message);
+		}
+		
 	});
 	
-    //create instance of master view
+    // Kickstart the application by creating an instance of LeagueView
     var tournaments = new LeagueView();
 
 } (jQuery));
