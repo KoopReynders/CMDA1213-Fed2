753f2a97578ff27deff025a8dd9fd8981f28cb39
joost <joost@maximonster.com>
2012-12-12 11:57:23 +0100

    worked on mvc6

--- Les 4/Lesopdrachten/Backbone Tournament Pages/js/app/app.js
+++ Les 4/Lesopdrachten/Backbone Tournament Pages/js/app/app.js
@@ -4,15 +4,15 @@ var FED2 = FED2 || {};
 
 // Define schedule data
 FED2.scheduleData = [
-	{ date: "Monday, 9:00am", team1: "Chasing", team1Score: "3", team2: "Amsterdam Money Gang", team2Score: "1"},
+	{ date: "Monday, 9:00am", team1: "Chasing", team1Score: "0", team2: "Amsterdam Money Gang", team2Score: "3"},
 	{ date: "Monday, 9:00am", team1: "Boomsquad", team1Score: "3", team2: "Beast Amsterdam", team2Score: "0"},
-	{ date: "Monday, 10:00am", team1: "Beast Amsterdam", team1Score: "3", team2: "Amsterdam Money Gang", team2Score: "0"},
+	{ date: "Monday, 10:00am", team1: "Beast Amsterdam", team1Score: "2", team2: "Amsterdam Money Gang", team2Score: "3"},
 	{ date: "Monday, 10:00am", team1: "Chasing", team1Score: "3", team2: "Burning Snow", team2Score: "0"},
 	{ date: "Monday, 11:00am", team1: "Boomsquad", team1Score: "3", team2: "Amsterdam Money Gang", team2Score: "0"},    
-	{ date: "Monday, 11:00am", team1: "Burning Snow", team1Score: "3", team2: "Beast Amsterdam", team2Score: "0"},
-	{ date: "Monday, 12:00pm", team1: "Chasing", team1Score: "3", team2: "Beast Amsterdam", team2Score: "0"},
+	{ date: "Monday, 11:00am", team1: "Burning Snow", team1Score: "2", team2: "Beast Amsterdam", team2Score: "3"},
+	{ date: "Monday, 12:00pm", team1: "Chasing", team1Score: "1", team2: "Beast Amsterdam", team2Score: "3"},
 	{ date: "Monday, 12:00pm", team1: "Boomsquad", team1Score: "3", team2: "Burning Snow", team2Score: "0"},
-	{ date: "Monday, 1:00pm", team1: "Chasing", team1Score: "3", team2: "Boomsquad", team2Score: "0"},
+	{ date: "Monday, 1:00pm", team1: "Chasing", team1Score: "1", team2: "Boomsquad", team2Score: "3"},
 	{ date: "Monday, 1:00pm", team1: "Burning Snow", team1Score: "3", team2: "Amsterdam Money Gang", team2Score: "0"}
 ];
 
--- Les 4/Lesopdrachten/Backbone Tournaments/js/app/app.js
+++ Les 4/Lesopdrachten/Backbone Tournaments/js/app/app.js
@@ -5,5 +5,5 @@ FED2.leagueData = [
 	{ name: "Threesome Ultimate Tournament", schedulingFormat:"regular"},
 	{ name: "Wisconsin Swiss 2012", schedulingFormat:"swiss"},
 	{ name: "New York City PoNY", schedulingFormat:"regular"},
-	{ name: "Swiss Chalet", schedulingFormat:"swiss"}
+	{ name: "Swiss Chalet"}
 ];
