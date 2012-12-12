d0868376c346adddf91acf7aa5a406bf52b923a3
joost <joost@maximonster.com>
2012-12-12 10:40:31 +0100

    Worked on tournament pages

---
+++ Les 4/Lesopdrachten/Backbone Tournament Pages/js/app/collections/pool.js
@@ -0,0 +1,10 @@
+// Define schedule collection
+FED2.Pool = Backbone.Collection.extend({
+    // Specifiy model for this collection
+	model: FED2.Team,
+	
+	// Initialize collection
+	initialize: function () {
+		
+	}
+});
---
+++ Les 4/Lesopdrachten/Backbone Tournament Pages/js/app/models/team.js
@@ -0,0 +1,13 @@
+// Define game model
+FED2.Team = Backbone.Model.extend({
+	// Set model defaults
+	defaults: {
+		
+	},
+	
+	// Initialize model
+	initialize: function () {
+		
+	}
+
+});
