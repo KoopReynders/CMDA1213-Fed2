5cd45c552e67a6f473cc69347ab7841b9e23b626
Joost Faber <Joost@Joosts-MacBook-Pro.local>
2012-12-18 21:31:47 +0100

    tweaks

--- Les 3/Lesopdrachten/Backbone Skeleton/js/app.js
+++ Les 3/Lesopdrachten/Backbone Skeleton/js/app.js
@@ -35,29 +35,29 @@ var ContactView = Backbone.View.extend({
     }
 });
 
-   //define master view
-    var DirectoryView = Backbone.View.extend({
-        el: $("#contacts"),
+//define master view
+var DirectoryView = Backbone.View.extend({
+    el: $("#contacts"),
 
-        initialize: function () {
-            this.collection = new Directory(contacts);
-            this.render();
-        },
+    initialize: function () {
+        this.collection = new Directory(contacts);
+        this.render();
+    },
 
-        render: function () {
-            var that = this;
-            _.each(this.collection.models, function (item) {
-                that.renderContact(item);
-            }, this);
-        },
+    render: function () {
+        var that = this;
+        _.each(this.collection.models, function (item) {
+            that.renderContact(item);
+        }, this);
+    },
 
-        renderContact: function (item) {
-            var contactView = new ContactView({
-                model: item
-            });
-            this.$el.append(contactView.render().el);
-        }
-    });
+    renderContact: function (item) {
+        var contactView = new ContactView({
+            model: item
+        });
+        this.$el.append(contactView.render().el);
+    }
+});
 
 
 var directory = new DirectoryView();
